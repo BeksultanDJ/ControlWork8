e296728f0998283107544c642c0c3c4e7d75590a
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-02 16:56:22 +0600

    написал функцию которая загружает данные из апи и интерфейс, так де была добавлена проверка на наличие ответа

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,45 @@
-import CategoryQuotes from "./CetegoryQuotes.tsx";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
+interface Quote {
+    author: string;
+    category: string;
+    text: string;
+}
 
-const Quotes = () => {
-    return(
+const Quotes: React.FC = () => {
+    const [quotes, setQuotes] = useState<Quote[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://controll-17843-default-rtdb.europe-west1.firebasedatabase.app/quotes.json');
+                if (response.data) {
+                    const quotesArray: Quote[] = Object.values(response.data);
+                    setQuotes(quotesArray);
+                }
+            } catch (error) {
+                console.error('Ошибка при получении цитат:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
         <div>
-            <h1>Hello</h1>
-            <CategoryQuotes/>
+            <h2>Цитаты</h2>
+            <ul>
+                {quotes.map((quote, index) => (
+                    <li key={index}>
+                        <p><strong>Author:</strong> {quote.author}</p>
+                        <p><strong>Category:</strong> {quote.category}</p>
+                        <p><strong>Text:</strong> {quote.text}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
-    )
+    );
 };
 
 export default Quotes;
