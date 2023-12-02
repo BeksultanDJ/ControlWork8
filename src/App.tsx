57529fae1014d5234db90f019bd3160386ebe2d1
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-02 18:25:33 +0600

    имортировал кноаку редактировани

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Quotes from "./components/Home.tsx";
 import NewQuotes from "./components/NewQuotes.tsx";
 import Toolbar from "./components/TollBar/ToolBar.tsx";
 import CategoryQuotes from "./components/CetegoryQuotes.tsx";
+import EditQuote from "./components/EditQuotePage.tsx";
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
               <Route path="/category/:categoryId" element={<CategoryQuotes/>}/>
               <Route path="/" element={<Quotes/>}/>
               <Route path="/NewQuotes" element={<NewQuotes />} />
+              <Route path="/:id/EditQuote" element={<EditQuote/>} />
           </Routes>
       </div>
     </>
